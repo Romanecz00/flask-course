21bea7323fe5408cc93fec05b4aed025ffc5694c
Roman <romanecz00@gmail.com>
2023-07-24 20:13:38 +0300

    instr. 2

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask
-
+import json
+from random import choice, randint
+from .datatypes import Person#, Quotary
 
 app = Flask(__name__)
 
-
-
+dataset = []
+for i in range(0, 1000):
+	dataset.append(Person({'id':i, 'name':chr(i)*10, 'age':randint(0,120), 'sex':'NullPointerException', 'location':'in your walls'}))
 
 @app.route("/")
 def hello_world():
-	return "<p>Hello, World!</p>"
+	rez = choice(dataset)
+	return json.dumps(rez.get(), indent=2)
+
+@app.route("/name")
+def n_return():
+	rez = choice(dataset).get()['name']
+	return json.dumps({'name':rez})
---
+++ datatypes.py
@@ -0,0 +1,47 @@
+import json
+from os import path
+from random import randint
+
+class Person:
+	data = {'id':'','name':'', 'age':'', 'sex':'', 'location':''}
+	
+	def __init__(self, stuff={'id':-1, 'name':'Test instance', 'age':randint(0,120), 'sex':'NullPointerException', 'location':'in your walls'}):
+		self.data = stuff
+	
+	def get(self):
+		return self.data
+	
+	def get_specific(self, mode='all'):
+		if mode.lower() in ['all','id','name','age','sex','location']:
+			return self.data[mode.lower()]
+		else:
+			return "wrong mode"
+	
+	def __cmp__(self, other):
+		if type(other) == type(self):
+			for k,v in self.data.items():
+				if not other[k] == v:
+					return False
+			return True
+		else:
+			return False
+
+# class Quotary:
+# 	data = [
+# 		{'id':'', 'author':'', 'content':''}
+# 		]
+# 	
+# 	def validate(self, stuff):
+# 		if type(stuff) == dict:
+# 			if ['name','author','content'].sort() == stuff.keys().sort():
+# 				pass
+# 		else:
+# 			return False
+# 	
+# 	def __init__(self, q=[{'id':'-1', 'author':'Debug Entity', 'content':'1337 69 420 71M3'}]):
+# 		if type(q) == list:
+# 			if 
+# 		elif type(q) == dict:
+# 			
+# 		else:
+# 			raise TypeError("wrong data")
