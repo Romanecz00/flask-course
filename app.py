f3e5603be0877a9c80e7019b6662fb09cb975dff
Roman <romanecz00@gmail.com>
2023-07-25 18:56:12 +0300

    practice #2

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import json
 from random import choice, randint
 from .datatypes import Person#, Quotary
@@ -22,12 +22,21 @@ def f_return(field):
 	else:
 		return "Error: no such field exists!\n", 404
 
-@app.route("/person/<int:_id>")
-def id_search(_id):
-	for i in dataset:
-		if i.get()['id'] == _id:
-			return i.get()
-	return "Error:No entity found with such ID!\n", 404
+@app.route("/person", methods=['POST'])
+def field_search():
+	data = request.json
+	if 'query' in data.keys() and data['query']:
+		if type(data['query'])==str and data['query'].lower() in Person().get().keys():
+			rez = choice(dataset).get()[data['query'].lower()]
+			return json.dumps({data['query']:rez})
+		else:
+			return "Error: no such field exists!\n", 404
+	else:
+		return "Error: no 'query' key in request, bad request.", 400
+	# for i in dataset:
+	# 	if i.get()['id'] == _id:
+	# 		return i.get()
+	# return "Error:No entity found with such ID!\n", 404
 	
 
 @app.route("/count")
