5e36dfad77073bac5cda03b81913d8ccf8235cb4
Roman <romanecz00@gmail.com>
2023-07-26 21:13:50 +0300

    lecture 4

--- app.py
+++ app.py
@@ -1,88 +1,101 @@
-from flask import Flask, request, json
+from flask import Flask, request, json, g
 import sqlite3 #json,
+from flask_sqlalchemy import SQLAlchemy
 from random import choice, randint
+from pathlib import Path
 from .datatypes import Person#, Quotary
 
 app = Flask(__name__)
+BASE_APP_DIR = Path(__file__).parent
+DB_DIR = BASE_APP_DIR / "test.db"
 json.provider.DefaultJSONProvider.ensure_ascii = False
 
-dataset = []
-for i in range(0, 1000):
-	dataset.append(Person({'id':i, 'name':chr(i)*10, 'age':randint(0,120), 'sex':'NullPointerException', 'location':'in your walls'}))
+# dataset = []
+# for i in range(0, 1000):
+# 	dataset.append(Person({'id':i, 'name':chr(i)*10, 'age':randint(0,120), 'sex':'NullPointerException', 'location':'in your walls'}))
+# 
+# @app.route("/")
+# def hello_world():
+# 	rez = choice(dataset)
+# 	return json.dumps(rez.get(), indent=2)
+# 
+# @app.route("/<field>")
+# def f_return(field):
+# 	if field.lower() in Person().get().keys():
+# 		rez = choice(dataset).get()[field.lower()]
+# 		return json.dumps({field:rez})
+# 	else:
+# 		return "Error: no such field exists!\n", 404
+# 
+# @app.route("/person", methods=['POST'])
+# def field_search():
+# 	data = request.json
+# 	for k,v in data.items():
+# 		if k in Person().fields and type(v) in (int, str, float):
+# 			for p in dataset:
+# 				if str(p.get()[k]) == str(v):
+# 					return json.dumps(p.get())
+# 			return f"Error: person with {k} of \"{v}\" was not found", 404
+# 		else:
+# 			return f"Error: incorrect query parameter {k}", 400
+# 	
+# @app.route('/edit/<int:_id>', methods=['PUT'])
+# def edit_person(_id):
+# 	data = request.json
+# 	for p in dataset:
+# 		if p.get()['id'] == _id:
+# 			for k,v in data.items():
+# 				if not k in p.fields:
+# 					return f"Error: wrong data field {k}", 400
+# 				try:
+# 					p._set(k, v)
+# 				except Exception as e:
+# 					return f"Internal Server Error", 500
+# 			return json.dumps(p.get())
+# 	return f"Error: no person with {_id} id found!\n", 404
+# 
+# @app.route('/birth', methods=['POST', 'PUT']) #because birth is the /create-person for those of us who aren't psychopaths
+# def create_person():
+# 	data = request.json
+# 	flag = True
+# 	for i in ['name', 'age', 'sex', 'location']:
+# 		if not i in data.keys():
+# 			flag = False
+# 		elif type(data[i]) not in (int, str, float):
+# 			flag = False
+# 	if flag:
+# 		dataset.append(Person({'id':len(dataset)+1, 'name':data['name'], 'age':data['age'], 'sex':data['sex'], 'location':data['location']}))
+# 		return json.dumps({'id':len(dataset), 'name':data['name'], 'age':data['age'], 'sex':data['sex'], 'location':data['location']})
+# 	return f'Error: invalid data. data: {data}\n', 400
+# 
+# @app.route("/count")
+# def get_count():
+# 	return {'count':len(dataset)}
 
-@app.route("/")
-def hello_world():
-	rez = choice(dataset)
-	return json.dumps(rez.get(), indent=2)
 
-@app.route("/<field>")
-def f_return(field):
-	if field.lower() in Person().get().keys():
-		rez = choice(dataset).get()[field.lower()]
-		return json.dumps({field:rez})
-	else:
-		return "Error: no such field exists!\n", 404
-
-@app.route("/person", methods=['POST'])
-def field_search():
-	data = request.json
-	for k,v in data.items():
-		if k in Person().fields and type(v) in (int, str, float):
-			for p in dataset:
-				if str(p.get()[k]) == str(v):
-					return json.dumps(p.get())
-			return f"Error: person with {k} of \"{v}\" was not found", 404
-		else:
-			return f"Error: incorrect query parameter {k}", 400
-	# if 'query' in data.keys() and data['query']:
-	# 	if type(data['query'])==str and data['query'].lower() in Person().get().keys():
-	# 		rez = choice(dataset).get()[data['query'].lower()]
-	# 		return json.dumps({data['query']:rez})
-	# 	else:
-	# 		return "Error: no such field exists!\n", 404
-	# else:
-	# 	return "Error: no 'query' key in request, bad request.", 400
-	# for i in dataset:
-	# 	if i.get()['id'] == _id:
-	# 		return i.get()
-	# return "Error:No entity found with such ID!\n", 404
-	
-@app.route('/edit/<int:_id>', methods=['PUT'])
-def edit_person(_id):
-	data = request.json
-	for p in dataset:
-		if p.get()['id'] == _id:
-			for k,v in data.items():
-				if not k in p.fields:
-					return f"Error: wrong data field {k}", 400
-				try:
-					p._set(k, v)
-				except Exception as e:
-					return f"Internal Server Error", 500
-			return json.dumps(p.get())
-	return f"Error: no person with {_id} id found!\n", 404
-
-@app.route('/birth', methods=['POST', 'PUT']) #because birth is the /create-person for those of us who aren't psychopaths
-def create_person():
-	data = request.json
+def validate(quote):
 	flag = True
-	for i in ['name', 'age', 'sex', 'location']:
-		if not i in data.keys():
-			flag = False
-		elif type(data[i]) not in (int, str, float):
-			flag = False
-	if flag:
-		dataset.append(Person({'id':len(dataset)+1, 'name':data['name'], 'age':data['age'], 'sex':data['sex'], 'location':data['location']}))
-		return json.dumps({'id':len(dataset), 'name':data['name'], 'age':data['age'], 'sex':data['sex'], 'location':data['location']})
-	return f'Error: invalid data. data: {data}\n', 400
+	if len(quote) == 3 and type(quote)==dict:
+		for k,v in quote.items():
+			if not type(v) in [dict, str, float]:
+				flag = False
+			if not k in ['name', 'id', 'quote']:
+				flag = False
+	else:
+		flag = False
+	return flag
 
-@app.route("/count")
-def get_count():
-	return {'count':len(dataset)}
-
-
-
-
+def get_db():
+	db = getattr(g, '_database', None)
+	if db is None:
+		db = g._database = sqlite3.connect(DB_DIR)
+	return db
+	
+@app.teardown_appcontext
+def close_connection():
+	db = getattr(g, '_database', None)
+	if db is not None:
+		db.close()
 
 def call_db(cmd, db='test.db'):
 	try:
@@ -134,13 +147,19 @@ def quotes_count():
 	return {'count':0}
 # 
 # 
-# @app.route("/quotes", methods=["POST"])
-# def create_quote():
-# 	new_quote = request.json
-# 	new_id = quotes[-1]["id"] + 1
-# 	new_quote["id"] = new_id
-# 	quotes.append(new_quote)
-# 	return new_quote, 201
+@app.route("/quotes/create", methods=["POST", "PUT"])
+def create_quote():
+	new_quote = request.json
+	if validate(new_quote):
+		connection = get_db()
+		cursor = connection.cursor()
+		sql = f"INSERT INTO quotes (author, text) VALUES ('{new_quote['name']}', '{new_quote['quote']}');"
+		cursor.execute(sql)
+		connection.commit()
+		new_quote["id"] = cursor.lastrowid
+		return new_quote, 201
+	else:
+		return f"Error: Wrong data/ data:\n{new_quote}\n"
 # 
 # 
 # @app.route("/quotes/<int:quote_id>", methods=['PUT'])
